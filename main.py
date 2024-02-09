ae4bdc43d70eea6ef340760f3c2511fa3e5dc145
LeviSecula <levi.secula@students.fhu.edu>
2024-02-09 09:16:31 -0600

    RePush

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ def anagram(a,b):
     newA = ''.join(sorted(a))
     newB = ''.join(sorted(b))
     return newA == newB
-# Fixed it?
+# Fixed it? maybe
+
 if __name__ == '__main__':
     a = input()
     b = input()
